d608b715a7b11e86d746ffbc1cb9f4e16fcbc6df
Sudip-Magar <sudeepmagar730@gmail.com>
2026-03-07 21:10:56 +0545

    start creating setup form

---
+++ app/Livewire/Forms/Student/StudentForm.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Livewire\Forms\Student;
+
+use App\Enums\StatusState;
+use Livewire\Attributes\Validate;
+use Livewire\Features\SupportFileUploads\WithFileUploads;
+use Livewire\Form;
+
+class StudentForm extends Form
+{
+    use WithFileUploads;
+    public $id;
+    public $admission_no;
+    public $first_name;
+    public $middle_name;
+    public $last_name;
+    public $photo;
+    public $old_photo;
+    public $gender;
+    public $date_of_birth_en;
+    public $date_of_birth_np;
+    public $email;
+    public $phone;
+    public $province_id;
+    public $district_id;
+    public $city;
+    public $ward_no;
+    public $admission_date_en;
+    public $admission_date_np;
+    public $status = StatusState::ACTIVE->name;
+}
--- app/Livewire/Scms/StudentSetup/Student/StudentAdd.php
+++ app/Livewire/Scms/StudentSetup/Student/StudentAdd.php
@@ -2,10 +2,37 @@
 
 namespace App\Livewire\Scms\StudentSetup\Student;
 
+use App\Enums\GenderState;
+use App\Enums\StatusState;
+use App\Livewire\Forms\Student\StudentForm;
 use Livewire\Component;
+use Livewire\Features\SupportFileUploads\WithFileUploads;
 
 class StudentAdd extends Component
 {
+    use WithFileUploads;
+    public $id;
+    public $title;
+    public StudentForm $studentForm;
+    public $status;
+    public $gender;
+
+    public function mount()
+    {
+        $this->gender = collect(GenderState::cases())
+            ->map(fn($item) => [
+                'value' => $item->name,
+                'label' => $item->value
+            ])
+            ->toArray();
+
+        if ($this->id) {
+            $this->title = "Edit Student ";
+        } else {
+            $this->title = 'Create Student';
+        }
+    }
+
     public function render()
     {
         return view('livewire.scms.student-setup.student.student-add');
